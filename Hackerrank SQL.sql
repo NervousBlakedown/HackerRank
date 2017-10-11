43eac6850116ab3a751cecbe3241da13129a63d9
Blake Calhoun <blakecalhoun35@gmail.com>
2017-10-11 06:33:00 -0500

    Add files via upload

--- Hackerrank SQL.sql
+++ Hackerrank SQL.sql
@@ -17,3 +17,8 @@ SELECT * FROM City WHERE countrycode = "USA" AND population > 100000;
 
 /*Binary Tree Nodes*/
 SELECT N, CASE WHEN P IS NULL THEN 'Root' WHEN (SELECT COUNT(*) FROM BST WHERE P=B.N)>0 THEN 'Inner' ELSE 'Leaf' END FROM BST AS B ORDER BY N;
+/*Advanced Select*/
+SELECT Company.Company_code, Company.founder, COUNT(DISTINCT Employee.Lead_Manager_code),
+COUNT(DISTINCT Employee.senior_manager_code), COUNT(DISTINCT Employee.manager_code),
+COUNT(DISTINCT Employee.employee_code) FROM Company INNER JOIN Employee ON Employee.Company_Code = Company.Company_Code, 
+GROUP BY Company.Company_Code, Company.founder ORDER BY Company.Company_code;
