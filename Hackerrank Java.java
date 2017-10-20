70c248cebad57f43899377e7bbe3d73943e7c21c
Blake Calhoun <blakecalhoun35@gmail.com>
2017-10-20 06:28:10 -0500

    Add files via upload

--- Hackerrank Java.java
+++ Hackerrank Java.java
@@ -92,3 +92,13 @@ public class Solution {
 
 
 }
+
+//Java Inheritance
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
+    void sing() {
+        System.out.println("I am singing");
+    }
+}
