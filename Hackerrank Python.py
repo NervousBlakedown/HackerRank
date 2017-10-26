5ca2169c05879de58459f6746f77b02c80182990
Blake Calhoun <blakecalhoun35@gmail.com>
2017-10-26 05:24:20 -0500

    Add files via upload

--- Hackerrank Python.py
+++ Hackerrank Python.py
@@ -93,3 +93,9 @@ u = int(input())
 print(v + u)
 print(v-u)
 print(v*u);
+
+#String Formatting
+n = int(raw_input())
+width = len("{0:b}".format(n))
+for i in xrange(1,n+1):
+  print "{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width);
