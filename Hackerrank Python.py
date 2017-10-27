7e4b9a3847e138de45a75263f05923ed77699c9b
Blake Calhoun <blakecalhoun35@gmail.com>
2017-10-27 06:28:23 -0500

    Add files via upload

--- Hackerrank Python.py
+++ Hackerrank Python.py
@@ -99,3 +99,11 @@ n = int(raw_input())
 width = len("{0:b}".format(n))
 for i in xrange(1,n+1):
   print "{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width);
+
+#Print function
+from __future__ import print_function
+import sys
+if __name__ == '__main__':
+    n = int(raw_input())
+    ls=[x for x in range(1,n+1)]
+    print(*ls,sep='',end='\n',file=sys.stdout);
