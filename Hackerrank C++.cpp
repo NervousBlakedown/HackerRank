1294cdb49b1fbd1866dfcd0a3e9821458fdecf20
Blake Calhoun <blakecalhoun35@gmail.com>
2018-10-15 16:31:24 -0500

    Add files via upload

--- Hackerrank C++.cpp
+++ Hackerrank C++.cpp
@@ -1,9 +1,47 @@
-//Basic Data Types
+// Basic Data Types.
 #include <iostream>
 #include <cstdio>
 using namespace std;
 
 int main() {
     printf("Hello, World!");
+    int a; long b; char c; float d; double e;
+cin>>a>>b>>c>>d>>e;
+cout<<a<<endl;
+cout<<b<<endl;
+cout<<c<<endl;
+cout.precision(3);
+cout<<fixed<<d<<endl;
+cout.precision(9);
+cout<<fixed<<e<<endl;
+return 0;
+}
+
+// Say "Hello, World!".
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
+
+// Input and Output.
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a,b,c,sum;
+
+cin>>a>>b>>c;
+sum=a+b+c;
+
+cout<<sum;
+
     return 0;
 }
