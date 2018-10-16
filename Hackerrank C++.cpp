a5f4a18ef12bcb93b33b7c911dfbd6f166c29f2c
Blake Calhoun <blakecalhoun35@gmail.com>
2018-10-16 07:09:51 -0500

    Add files via upload

--- Hackerrank C++.cpp
+++ Hackerrank C++.cpp
@@ -45,3 +45,40 @@ cout<<sum;
 
     return 0;
 }
+
+// Conditional Statements.
+int main() {
+int in;
+string num[10] = {"Greater than 9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+cin >> in;
+
+if(in > 9){
+    cout << num[0];
+}
+else{
+    cout << num[in];
+}
+
+return 0;
+}
+
+// For Loop.
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int i,c,b;
+    string a[11]={"even","odd","one","two","three","four","five","six","seven","eight","nine"};
+    cin>>c>>b;
+    for(i=c;i<=b;i++)
+    {
+        if((i>9) && (i%2==0))  cout<<a[0]<<endl;
+        else if((i>9) && (i%2!=0)) cout<<a[1]<<endl;
+        else    cout<<a[i+1]<<endl;
+    }
+    return 0;
+}
+
+// Functions.
